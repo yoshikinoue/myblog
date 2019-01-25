2c0baebed4897d12930bcbed24dd923aa6869644
Yoshiki Inoue <yoshiki@anemone.local>
2019-01-25 12:03:14 +0900

    navbar 消し忘れ

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -49,9 +49,6 @@ const Navbar = class extends React.Component {
           <Link className="navbar-item" to="/about">
             About
           </Link>
-          <!--Link className="navbar-item" to="/products">
-            Products
-          </Link-->
         </div>
       </div>
     </div>
