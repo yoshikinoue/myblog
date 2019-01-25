0da1483c015d706eed0efd02b935d36d7fe1e768
Yoshiki Inoue <yoshiki@anemone.local>
2019-01-25 11:33:37 +0900

    navbar 不要なもの削除

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -45,32 +45,14 @@ const Navbar = class extends React.Component {
         </div>
       </div>
       <div id="navMenu" className="navbar-menu">
-      <div className="navbar-start has-text-centered">
-        <Link className="navbar-item" to="/about">
-          About
-        </Link>
-        <Link className="navbar-item" to="/products">
-          Products
-        </Link>
-        <Link className="navbar-item" to="/contact">
-          Contact
-        </Link>
-        <Link className="navbar-item" to="/contact/examples">
-          Form Examples
-        </Link>
-      </div>
-      <div className="navbar-end has-text-centered">
-        <a
-          className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span className="icon">
-            <img src={github} alt="Github" />
-          </span>
-        </a>
-      </div>
+        <div className="navbar-start has-text-centered">
+          <Link className="navbar-item" to="/about">
+            About
+          </Link>
+          <!--Link className="navbar-item" to="/products">
+            Products
+          </Link-->
+        </div>
       </div>
     </div>
   </nav>
