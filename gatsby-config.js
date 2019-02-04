12cea08e8180281e01871d7bd14a95f140091fbc
yoshikinoue <ino.yoshiki+github@gmail.com>
2019-02-04 15:09:28 +0900

    gaの設定変更

--- gatsby-config.js
+++ gatsby-config.js
@@ -91,13 +91,13 @@ module.exports = {
       options: {
         trackingId: "UA-133365008-1",
         // Puts tracking script in the head instead of the body
-        head: false,
+        //head: false,
         // Setting this parameter is optional
-        anonymize: true,
+        //anonymize: true,
         // Setting this parameter is also optional
-        respectDNT: true,
+        //respectDNT: true,
         // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        //exclude: ["/preview/**", "/do-not-track/me/too/"],
         // Enables Google Optimize using your container Id
         //optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
         // Enables Google Optimize Experiment ID
@@ -105,8 +105,8 @@ module.exports = {
         // Set Variation ID. 0 for original 1,2,3....
         //variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
         // Any additional create only fields (optional)
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
+        //sampleRate: 5,
+        //siteSpeedSampleRate: 10,
         //cookieDomain: "example.com",
       },
     },
