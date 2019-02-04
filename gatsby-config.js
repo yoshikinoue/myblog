f7976c6c3663ebd91791738c92bd7d19dcaf1e86
yoshikinoue <ino.yoshiki+github@gmail.com>
2019-02-04 10:46:06 +0900

    ga追加

--- gatsby-config.js
+++ gatsby-config.js
@@ -86,5 +86,29 @@ module.exports = {
     {
       resolve: `gatsby-plugin-feed`
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-133365008-1",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Enables Google Optimize using your container Id
+        //optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+        // Enables Google Optimize Experiment ID
+        //experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
+        // Set Variation ID. 0 for original 1,2,3....
+        //variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
+        // Any additional create only fields (optional)
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        //cookieDomain: "example.com",
+      },
+    },
   ],
 }
